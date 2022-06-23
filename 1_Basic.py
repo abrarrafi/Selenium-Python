847cb5ff971b8b2b3d30075e9279bc661b7a627c
Abrar Shahriar <abrar.shahriar1809@gmail.com>
2022-06-23 16:20:56 +0600

    1_Basic: Added simple web navigation and settings

--- 1_Basic.py
+++ 1_Basic.py
@@ -1,3 +1,13 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
+
+driver.get('https://www.google.com')
+
+# Web title, url and source code
+print(driver.title)
+print(driver.current_url)
+print(driver.page_source)
+
+# window maximize
+driver.maximize_window()
